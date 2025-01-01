c425b07c2943d662984308f19e609004b11c2e93
Mats-Ola Persson <matsola.persson@gmail.com>
2025-01-01 13:13:04 +0100

    Adjustments

--- copy-images-to-nas.sh
+++ copy-images-to-nas.sh
@@ -2,19 +2,19 @@
 
 IMAGE_DIR=/home/pi/timelapser/images
 
-TMP_DIR_PATTERN=/tmp/images-
+TMP_DIR_PATTERN=/tmp/images
 TMP_DIR=$TMP_DIR_PATTERN-$(date '+%Y%m%d-%H%M%S')
 TAR_NAME=/tmp/images-$(date '+%Y%m%d-%H%M%S').tar
 
 mv $IMAGE_DIR $TMP_DIR
 mkdir $IMAGE_DIR
-
-tar cf $TAR_NAME $TMP_DIR
+ 
+tar cf $TAR_NAME -C $TMP_DIR .
 rm -rf $TMP_DIR
 
 for f in "$TMP_DIR_PATTERN"*tar; do 
     echo "Uploading $f"
-    echo "put $TAR_NAME $2" | sshpass -f ~/.nas-password sftp $1
+    echo "put $f $2" | sshpass -f ~/.nas-password sftp $1
 
     if [ "$?" -eq "0" ]; then 
         echo "File $f uploaded"
